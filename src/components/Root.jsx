3c60e07e2a7e8d1c99581bc106787455bb69d55f
Xagly Montilva <xaglymontilva@gmail.com>
2023-09-23 19:53:25 +0200

    updated to use Context for fetching accross app

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,14 +1,118 @@
 import React from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { Navigation } from "./Navigation";
 import { NavigationFooter } from "./NavigationFooter";
+import NewEvent from "./form/NewEvent";
+import { useToast } from "@chakra-ui/react";
 
-export const Root = ({ events, setFilteredEvents }) => {
+//creating context for data
+const DataContext = createContext();
+
+//manage and provide data
+export const Root = ({ initialEvents, setFilteredEvents, children }) => {
+  const [eventsData, setEventsData] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [description, setDescription] = useState([]);
+
+  const toast = useToast();
+
+  const handleFilteredEvents = (searchValue) => {
+    if (searchValue === "") {
+      setFilteredEvents(eventsData);
+    } else {
+      const matchedEvents = eventsData.filter((event) => {
+        const { location, title } = event;
+        return (
+          location.toLowerCase().includes(searchValue.toLowerCase()) ||
+          title.toLowerCase().includes(searchValue.toLowerCase())
+        );
+      });
+      setFilteredEvents(matchedEvents);
+    }
+  };
+
+  //Fetching data from API
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const eventsResponse = await fetch("http://localhost:3000/events");
+        const eventsData = await eventsResponse.json();
+        setEventsData(eventsData);
+        setFilteredEvents(eventsData);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  //moved and modified the following piece of code from EventsPage
+  const handleEventAdded = async (NewEventData) => {
+    try {
+      //send POST
+      const newEventResponse = await fetch("http://localhost:3000/events", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(NewEventData),
+      });
+
+      if (newEventResponse.ok) {
+        const updatedEventsResponse = await fetch(
+          "http://localhost:3000/events"
+        );
+        const updatedEvnts = await updatedEventsResponse.json();
+        setEventsData(updatedEvnts);
+
+        toast({
+          title: "Event Created",
+          description: "Your event has been successfuly created!",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "Error",
+          description: "Failed to create new event. Please try again later",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+    } catch (error) {
+      console.error("Error adding event:", error);
+    }
+  };
+
+  const contextValue = {
+    events: eventsData,
+    categories,
+    users,
+    description,
+    showToast: toast,
+    handleFilteredEvents,
+    handleEventAdded,
+  };
+
   return (
-    <>
-      <Navigation events={events} setFilteredEvents={setFilteredEvents} />
-      <Outlet />
+    <DataContext.Provider value={contextValue}>
+      <Navigation />
+      {children}
       <NavigationFooter />
-    </>
+    </DataContext.Provider>
   );
 };
+
+// export const Root = ({ events, setFilteredEvents }) => {
+//   return (
+//     <>
+//       <DataProvider>
+//         <Navigation events={events} setFilteredEvents={setFilteredEvents} />
+//         <Outlet />
+//         <NavigationFooter />
+//       </DataProvider>
+//     </>
+//   );
+// };
