814765103207701566c21a4cafa2e4164953b0dc
Xagly Montilva <xaglymontilva@gmail.com>
2023-09-26 22:21:07 +0200

    comments?

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createContext, useState, useEffect } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { Navigation } from "./Navigation";
 import { NavigationFooter } from "./NavigationFooter";
