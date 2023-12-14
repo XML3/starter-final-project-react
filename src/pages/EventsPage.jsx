797c8911fed7903ca7b6045674a62ccfbea5fef1
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-15 00:10:33 +0100

    modified the entire design of the page, functionality stays the same

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -4,6 +4,7 @@ import { EventsCard } from "../components/EventsCard";
 import NewEvent from "../components/form/NewEvent";
 import { SearchItem } from "../components/SearchItem";
 import DataContext from "../components/Root";
+import ImgAnimation from "../components/ImgAnimation";
 
 import {
   Heading,
@@ -20,7 +21,9 @@ import {
   ModalCloseButton,
   useBreakpointValue,
   Button,
+  Center,
 } from "@chakra-ui/react";
+import ImgLoop from "../components/ImgLoop";
 
 export const EventsPage = () => {
   const {
@@ -41,39 +44,71 @@ export const EventsPage = () => {
     setIsModalOpen(false);
   };
 
-  const columns = useBreakpointValue({ base: 1, sm: 1, md: 2, lg: 2 });
+  const columns = useBreakpointValue({ base: 1, sm: 2, md: 3, lg: 4 });
 
   return (
     <>
       <Box bgColor="gray.900" color="whitesmoke" minH="100vh">
-        <Heading color={"green.200"} ml={"1.5rem"} fontSize={"200px"}>
-          Electronic Music Events
-        </Heading>
+        <Flex
+          align={{ base: "center", md: "flex-start" }}
+          justify={"center"}
+          minH={"100vh"}
+          direction={{ base: "column", md: "row" }}
+        >
+          <Box ml={{ base: "0", md: "1.5rem" }} mr={{ base: "0", md: "11rem" }}>
+            <Heading
+              color={"green.200"}
+              // ml={"1.5rem"}
+              fontSize={{ base: "100px", md: "130px" }}
+              lineHeight={"1.2"}
+              mt={{ base: "1rem", md: "0" }}
+              ml={{ base: "0.5rem", md: "0" }}
+            >
+              Electronic Music <br />
+              Events
+            </Heading>
+            {/* </Flex> */}
+            {/* <Flex wrap={"wrap"} justify="center" minH="2vh" maxW={"40rem"}> */}
+            {/* <Box mt={"3rem"}> */}
+
+            <Text
+              color={"gray.300"}
+              ml={{ base: "0.5rem", md: "0.5rem" }}
+              fontSize={{ base: "18px", md: "20px" }}
+              mt={{ base: "1rem", md: "1rem" }}
+              maxW={{ base: "100%", md: "45rem" }}
+              marginBottom={{ base: "2rem", md: "0" }}
+            >
+              We aim to provide information on Electronic Music Events in your
+              area by offering our service to all promoters for different types
+              of venues and size events in all corners of the world to enrich
+              human lives through movement and sound.
+            </Text>
+          </Box>
+          <ImgAnimation />
+        </Flex>
+        {/* 
+        <Center>
+          <ImgLoop />
+        </Center> */}
 
         <Flex align="center" justify="center" minH="100vh">
-          <Image
-            h="30rem"
-            w="45rem"
-            mr={90}
-            ml={"3rem"}
-            src="/src/img/path13.png"
-            alt="geometrical pattern"
-          />
-
           <div className="events-page">
-            <Text fontSize={"2rem"} color={"yellow.200"}>
+            {/* <Text fontSize={"1.5rem"} color={"yellow.200"}>
               Search Events:
               <SearchItem
                 events={events}
                 handleFilteredEvents={handleFilteredEvents}
               />
-            </Text>
+            </Text> */}
             {/* Modal Form */}
             <Button
               onClick={openModal}
               bgColor={"green.200"}
               color={"gray.900"}
-              mb={10}
+              ml={{ base: "6rem", md: "5rem" }}
+              mb={{ base: "2rem", md: "5rem" }}
+              mt={{ base: "2rem", md: "-40rem" }}
             >
               {" "}
               Add New Event
@@ -100,14 +135,36 @@ export const EventsPage = () => {
             <Box
               p={4}
               bgColor={"gray.500"}
-              w={"35rem"}
-              h={"40rem"}
+              w={"100wh"}
+              h={"100vh"}
               borderRadius={"md"}
               mr={"2rem"}
               mb={"3rem"}
               padding={"2rem"}
               overflowY="scroll"
+              position={"relative"}
+              mt={{ base: "2rem", md: "-20rem" }}
             >
+              {/* Search Event Input */}
+              <Box
+                position={"sticky"}
+                top={0}
+                right={0}
+                mt={4}
+                mr={4}
+                zIndex={1}
+                p={4}
+              >
+                <Text fontSize={"1.5rem"} color={"yellow.200"}>
+                  Search Events:
+                  <SearchItem
+                    events={events}
+                    handleFilteredEvents={handleFilteredEvents}
+                  />
+                </Text>
+              </Box>
+
+              {/* Events Cards */}
               <Flex align={"center"} justify={"center"}>
                 <SimpleGrid columns={columns} gap={8}>
                   {/* //map through filtered events/+ Search functionality - Root component/ SeearchItem component*/}
