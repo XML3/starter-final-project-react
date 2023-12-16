d24f00a8062078454918f40ce4df52e21699e9e6
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-16 14:55:28 +0100

    modified nav structure and background colour.  Changed borderBottom thinkness

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
-import { Box, Heading, Flex } from "@chakra-ui/react";
+import { Box, Heading, Flex, Image } from "@chakra-ui/react";
 
 export const Navigation = () => {
   return (
     <Box
-      borderBottom={"4px"}
+      borderBottom={"2px"}
       borderColor="gray.700"
       bgColor={"gray.900"}
-      align={"right"}
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      zIndex={1000} // Set a high zIndex to ensure it stays on top
+      // align={"right"}
       p={4}
     >
-      <Flex justify={"flex-end"}>
-        <Heading color={"pink.500"} size="md" mr={8}>
+      <Flex
+        direction={{ base: "column", md: "row" }}
+        align={{ base: "start", md: "center" }}
+      >
+        <Heading
+          color="pink.500"
+          size="md"
+          mb={{ base: 2, md: 0 }}
+          mr={{ base: 0, md: 8 }}
+        >
           <Link to="/">Home</Link>
         </Heading>
 
@@ -21,6 +34,16 @@ export const Navigation = () => {
           </Heading>
         </Link>
       </Flex>
+      {/* <Box>
+        <Flex justify={"flex-end"}>
+          <Image
+            src="../src/img/Drun_n_bass.png"
+            boxSize="50px"
+            objectFit="cover"
+            borderRadius="full"
+          />
+        </Flex>
+      </Box> */}
     </Box>
   );
 };
