119880e5bf811d8badaa7f3c0be05cf5ce4fc642
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-20 17:36:05 +0100

    change design on the page, added personal logo

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -13,7 +13,6 @@ import {
   FormControl,
   FormLabel,
   FormErrorMessage,
-  FormHelperText,
 } from "@chakra-ui/react";
 
 //FONT ORBITRON
@@ -119,9 +118,9 @@ const ContactForm = () => {
         <Text
           color={"pink.500"}
           // ml={"1.5rem"}
-          fontSize={{ base: "10px", md: "10px", lg: "25px", xl: "25px" }}
-          lineHeight={"1.2"}
+          fontSize={{ base: "15px", md: "10px", lg: "25px", xl: "25px" }}
           mt={{ base: "1rem", md: "0" }}
+          mb={{ base: "1rem", md: 0 }}
           ml={{ base: "0.5rem", md: "0" }}
           fontFamily={orbitronFontFamily}
           fontWeight={orbitronWeight.semibold}
@@ -130,98 +129,133 @@ const ContactForm = () => {
         </Text>
       </Box>
 
-      <Box bgColor="gray.900" color="whitesmoke" maxW="xl" mx={"auto"} p={4}>
-        {/* FORM */}
-        <form onSubmit={handleSubmit}>
-          {/* NAME */}
-          <FormControl id="name" mt={"1rem"} isRequired>
-            <FormLabel
-              fontFamily={robotoSlabFont}
-              fontWeight={robotoSlabWeight.regular}
-            >
-              Name
-            </FormLabel>
-            <Input
-              placeholder="Full Name"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              focusBorderColor="pink.500"
-              style={{ borderColor: isNameError ? "red.500" : "inherit" }}
-            />
-            {isNameError && (
-              <FormErrorMessage>Name is required</FormErrorMessage>
-            )}
-          </FormControl>
-
-          {/* EMAIL */}
-          <FormControl id="email" isInvalid={isEmailError} isRequired>
-            <FormLabel
-              fontFamily={robotoSlabFont}
-              fontWeight={robotoSlabWeight.regular}
-              mt={"1rem"}
-            >
-              Email
-            </FormLabel>
-            <Input
-              placeholder="Email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              focusBorderColor="pink.500"
-              style={{ borderColor: isEmailError ? "red.500" : "inherit" }}
-            />
-            {isEmailError && (
-              <FormErrorMessage>Email is required</FormErrorMessage>
-            )}
-          </FormControl>
-
-          {/* MESSAGE */}
-          <FormControl id="message" isRequired>
-            <FormLabel
-              fontFamily={robotoSlabFont}
-              fontWeight={robotoSlabWeight.regular}
-              mt={"1rem"}
-            >
-              Message
-            </FormLabel>
-            <Textarea
-              placeholder="Write us a message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              focusBorderColor="pink.500"
-              style={{ borderColor: isMessageError ? "red.500" : "inherit" }}
-            />
-            {isMessageError && (
-              <FormErrorMessage>Message is required</FormErrorMessage>
-            )}
-          </FormControl>
-
-          {/* BUTTON */}
-          <Button
-            type="submit"
-            bgColor={"gray.900"}
-            color={"green.200"}
-            fontFamily={orbitronFontFamily}
-            fontWeight={orbitronWeight.medium}
-            mt={"2rem"}
-            mb={"10rem"}
-            fontSize={{ base: "0.8rem", md: "0.8rem" }}
-            //shadow
-            border="1px solid"
-            borderColor={"green.200"}
-            boxShadow=" 0px 2px 9px 0px whitesmoke"
-            _hover={{
-              bgColor: "green.200",
-              color: "gray.900",
-              boxShadow: "0 0 7px whitesmoke",
-            }}
-            _active={{ boxShadow: "0px 10px 30px 0px whitesmoke" }}
+      <Center>
+        <Box
+          bgColor="gray.900"
+          color="whitesmoke"
+          // mx={"auto"}
+          // p={4}
+          border="1px solid"
+          borderColor={"gray.700"}
+          w={{ base: "90%", md: "50%" }}
+          // h={{ base: "100%", md: "50vh" }}
+          padding={{ base: "1rem", md: "5rem" }}
+          mb={"5rem"}
+        >
+          <Flex
+            direction={{ base: "column", md: "row" }}
+            align={{ base: "center", md: "flex-start" }}
+            wrap={"wrap"}
           >
-            Submit
-          </Button>
-        </form>
-      </Box>
+            <Image
+              src="../src/img/xagly_logo_nobg.png"
+              alt="personal logo in pink"
+              marginRight={{ base: "0", md: "10rem" }}
+              position={"relative"}
+              top={{ base: "0.5", md: "5rem" }}
+              objectFit={{ base: "contain", md: "contain" }}
+              size={{ base: "40px", md: "200px" }}
+              maxW={{ base: "20%", md: "50%" }}
+            />
+            {/* FORM */}
+            <form onSubmit={handleSubmit}>
+              {/* NAME */}
+              <FormControl
+                id="name"
+                mt={"1rem"}
+                w={{ base: "12rem", md: "20rem" }}
+                isRequired
+              >
+                <FormLabel
+                  fontFamily={robotoSlabFont}
+                  fontWeight={robotoSlabWeight.regular}
+                >
+                  Name
+                </FormLabel>
+                <Input
+                  placeholder="Full Name"
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  focusBorderColor="pink.500"
+                  style={{ borderColor: isNameError ? "red.500" : "inherit" }}
+                />
+                {isNameError && (
+                  <FormErrorMessage>Name is required</FormErrorMessage>
+                )}
+              </FormControl>
+
+              {/* EMAIL */}
+              <FormControl id="email" isInvalid={isEmailError} isRequired>
+                <FormLabel
+                  fontFamily={robotoSlabFont}
+                  fontWeight={robotoSlabWeight.regular}
+                  mt={"1rem"}
+                >
+                  Email
+                </FormLabel>
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  focusBorderColor="pink.500"
+                  style={{ borderColor: isEmailError ? "red.500" : "inherit" }}
+                />
+                {isEmailError && (
+                  <FormErrorMessage>Email is required</FormErrorMessage>
+                )}
+              </FormControl>
+
+              {/* MESSAGE */}
+              <FormControl id="message" isRequired>
+                <FormLabel
+                  fontFamily={robotoSlabFont}
+                  fontWeight={robotoSlabWeight.regular}
+                  mt={"1rem"}
+                >
+                  Message
+                </FormLabel>
+                <Textarea
+                  placeholder="Write us a message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  focusBorderColor="pink.500"
+                  style={{
+                    borderColor: isMessageError ? "red.500" : "inherit",
+                  }}
+                />
+                {isMessageError && (
+                  <FormErrorMessage>Message is required</FormErrorMessage>
+                )}
+              </FormControl>
+
+              {/* BUTTON */}
+              <Button
+                type="submit"
+                bgColor={"gray.900"}
+                color={"green.200"}
+                fontFamily={orbitronFontFamily}
+                fontWeight={orbitronWeight.medium}
+                mt={"2rem"}
+                fontSize={{ base: "0.8rem", md: "0.8rem" }}
+                //shadow
+                border="1px solid"
+                borderColor={"green.200"}
+                boxShadow=" 0px 2px 9px 0px whitesmoke"
+                _hover={{
+                  bgColor: "green.200",
+                  color: "gray.900",
+                  boxShadow: "0 0 7px whitesmoke",
+                }}
+                _active={{ boxShadow: "0px 10px 30px 0px whitesmoke" }}
+              >
+                Submit
+              </Button>
+            </form>
+          </Flex>
+        </Box>
+      </Center>
     </Box>
   );
 };
