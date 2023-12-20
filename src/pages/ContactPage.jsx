9ab10965e937721c41ee972e585d03f806b824f4
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-20 22:36:58 +0100

    changed logo

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -148,7 +148,7 @@ const ContactForm = () => {
             wrap={"wrap"}
           >
             <Image
-              src="../src/img/xagly_logo_nobg.png"
+              src="../src/img/xagly_logo_nobg_dark2.png"
               alt="personal logo in pink"
               marginRight={{ base: "0", md: "10rem" }}
               position={"relative"}
